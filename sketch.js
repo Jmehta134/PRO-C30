011dca1baac4d17d543a0205f4fbdbbd7742b712
swajaydeep@gmail.com <6575@jay>
2021-05-25 15:40:15 +0530

    2nd pyramid created

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var stand;
 var block1,block2,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16;
+var Block1,Block2,Block3,Block4,Block5,Block6,Block7,Block8,Block9;
 var Slingshot;
 var polygon;
 function preload(){
@@ -15,10 +16,11 @@ function setup() {
   var canvas = createCanvas(800,400);
   engine = Engine.create();
   world = engine.world;
-  // stand  
-  stand = new Ground(390,300,300,10);
+  // stand  (both)
+  stand = new Ground(390,300,220,10);
+  stand2 = new Ground(590,235,150,10);
 
-  //blocks//
+  //blocks on right //
   //level 1st
   block1 = new Block(300,275,30,40);
   block2 = new Block(330,275,30,40);
@@ -38,7 +40,23 @@ function setup() {
   block14 = new Block(390,195,30,40);
   block15 = new Block(420,195,30,40);
   //level 4
-  Block16 = new Block(390,155,30,40);
+  block16 = new Block(390,155,30,40);
+  
+
+  // Blocks on left//
+  // level 1
+  Block1 = new Block(530,210,30,40);
+  Block2 = new Block(560,210,30,40);
+  Block3 = new Block(590,210,30,40);
+  Block4 = new Block(620,210,30,40);
+  Block5 = new Block(650,210,30,40);
+  //level 2
+  Block6 = new Block(560,170,30,40);
+  Block7 = new Block(590,170,30,40);
+  Block8 = new Block(620,170,30,40);
+  //level 3
+  Block9 = new Block(590,130,30,40);
+
 
   //polygon
   var options = {
@@ -57,6 +75,8 @@ function draw() {
   background(255,255,255);  
   Engine.update(engine);
   stand.display();
+  stand2.display();
+
   block1.display();
   block2.display();
   block3.display();
@@ -72,7 +92,18 @@ function draw() {
   block13.display();
   block14.display();
   block15.display();
-  Block16.display();
+  block16.display();
+  //left blocks display
+  Block1.display();
+  Block2.display();
+  Block3.display();
+  Block4.display();
+  Block5.display();
+  Block6.display();
+  Block7.display();
+  Block8.display();
+  Block9.display();
+
   Slingshot.display();
   fill(0,0,255);
   ellipse(polygon.position.x,polygon.position.y,40,40);
